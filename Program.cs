e2abee261f9cef8d73ccfe4e3f0744513ff26b54
ShendygaevDmitriy <129860994+ShendygaevDmitriy@users.noreply.github.com>
2023-04-13 23:00:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// Console.WriteLine("Введите первое число  ");
+// int a = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число  ");
+// int b = int.Parse(Console.ReadLine());
+
+// if (a > b) {
+//      Console.WriteLine("Первое число " + a + " больше чем второе " + b);
+// } else {
+//    Console.WriteLine("Второе число " + b + " больше чем первое " + a);
+// }
+
+
+
+// Console.WriteLine("Введите 3 числа:");
+// int a = int.Parse(Console.ReadLine());
+// int b= int.Parse(Console.ReadLine());
+// int c = int.Parse(Console.ReadLine());
+
+// int max = a;
+
+// if (b > max)
+// {
+// max = b;
+// }
+
+// if (c > max)
+// {
+//     max = c;
+// }
+
+// Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+
+
+// Console.WriteLine("Введите число");
+// int a = int.Parse(Console.ReadLine());
+
+// if (a % 2 == 1){
+//     Console.WriteLine("Число " + a + " нечётное");
+// }
+// else {
+//     Console.WriteLine("Число "+ a +"  чётное");
+// }
+
+
+
+
+
+// int i = 1;
+// bool not = true;
+
+// System.Console.WriteLine("Введите N: ");
+// int s = int.Parse(Console.ReadLine());
+// Console.WriteLine("Чётные числа от 1 до " + s);
+// while (i <= s)
+// {
+//     if (i % 2 !=1)
+//     {
+//          Console.Write(i + ", ");
+//                     not = false;
+//     }
+//     i++;
+// }
+
+// if (not)
+// {
+//     Console.WriteLine("Нет четных чисел");
+// }
+
